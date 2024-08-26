82fb29055def98967932e84ad143902c31fce319
kanadome <tsuchiyakeita2025@gmail.com>
2024-08-26 22:28:18 +0900

    解説記事部分のデザインを変更

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 interface ArticleProps {
   algorithmData: string
@@ -9,7 +11,33 @@ const Article: React.FC<ArticleProps> = ({ algorithmData }) => {
   return (
     <div className='bg-gray-800 p-6 rounded-lg'>
       <div className='prose prose-invert prose-lg max-h-[70vh] overflow-y-auto pr-4'>
-        <ReactMarkdown>{algorithmData}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            code({
+              inline,
+              className,
+              children,
+              ...props
+            }: {
+              inline?: boolean
+              className?: string
+              children: React.ReactNode
+            }) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag='div' {...props}>
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            },
+          }}
+        >
+          {algorithmData}
+        </ReactMarkdown>
       </div>
     </div>
   )
