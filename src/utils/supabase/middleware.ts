4cacb047bb899df43cca06dfc65b249c5f36ee46
ogane0112 <kai513272@gmail.com>
2024-08-28 20:44:04 +0900

    supabase関連に適切に静的型付けを行いました

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -2,8 +2,6 @@ import { createServerClient } from '@supabase/ssr'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export const updateSession = async (request: NextRequest) => {
-  // This `try/catch` block is only here for the interactive tutorial.
-  // Feel free to remove once you have Supabase connected.
   try {
     // Create an unmodified response
     let response = NextResponse.next({
@@ -38,7 +36,15 @@ export const updateSession = async (request: NextRequest) => {
     await supabase.auth.getUser()
 
     return response
-  } catch (e) {
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error('Failed to create Supabase client:', e.message)
+      // ここで特定のエラータイプに応じた処理を行うこともできます
+      // 例: if (e instanceof TypeError) { ... }
+    } else {
+      console.error('An unknown error occurred:', e)
+    }
+
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
