c49b9a1c67f196e1ef6efc5a7a1d880dc032409b
ogane0112 <kai513272@gmail.com>
2024-09-01 11:35:42 +0900

    　細かい修正点を直しました

--- src/components/search/actions.ts
+++ src/components/search/actions.ts
@@ -9,7 +9,7 @@ export async function searchVideos(formData: FormData): Promise<void> {
   const query = formData.get('query') as string
 
   if (!query) {
-    throw new Error('Query is required');
+    throw new Error('Query is required')
   }
   redirect(`/explore?query=${encodeURIComponent(query)}`)
 }
